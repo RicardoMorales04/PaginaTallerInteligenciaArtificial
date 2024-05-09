44946046846a1ccef5c6c5a6a310357d5980505e
RicardoMorales04 <ricardoms.ti22@utsjr.edu.mx>
2024-05-09 13:24:53 -0600

    Paginas de Blog y Registro

--- app/layout.jsx
+++ app/layout.jsx
@@ -30,6 +30,24 @@ export default function RootLayout({ children }) {
         </div>
 
         {children}
+        <footer>
+            <div className="contenedor-mapa">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d8518.983113962704!2d-100.01360384677483!3d20.36759990579274!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x16daa5dc56c8c592!2sUniversidad%20Tecnol%C3%B3gica%20de%20San%20Juan%20del%20R%C3%ADo!5e0!3m2!1ses!2smx!4v1657510541631!5m2!1ses!2smx"
+                    style={{ border: 0, width: '600px', height: '450px', borderRadius: '20px' }} // Aquí está el objeto de estilo
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    title="Mapa de ubicación"
+                ></iframe>
+            </div>
+            <div className="direccion">
+                <p>Av. La Palma No. 125, Col. Vista Hermosa | San Juan del Río, Qro. | Tel. (427) 129 20 00 | C.P. 76800</p>
+            </div>
+            <div  className="aviso-privacidad">
+                <a href="/docs/avisoprivacidad.pdf" target="_blank">Aviso de privacidad</a>
+            </div>
+            </footer>
       </body>
     </html>
   );
--- app/registro/page.jsx
+++ app/registro/page.jsx
@@ -1,5 +1,34 @@
+import "../css/registro.css"
+
 export default function Registro(){
     return(
-        <h1>Hello reg</h1>
+        <div class="container">
+            <h1>Registro de Talleres</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Profesor</th>
+                        <th>Taller</th>
+                        <th>Horario</th>
+                        <th>Formulario</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Profesor 1</td>
+                        <td>Taller de HTML</td>
+                        <td>Lunes 9:00 - 11:00</td>
+                        <td><a href="link_al_formulario">Registrarse</a></td>
+                    </tr>
+                    <tr>
+                        <td>Profesor 2</td>
+                        <td>Taller de CSS</td>
+                        <td>Miércoles 14:00 - 16:00</td>
+                        <td><a href="link_al_formulario">Registrarse</a></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
+
